61ceab9a86e9ffa6f6231a44fa70d03117ae3897
JohannSeverin <johannseverin@DESKTOP-U5QQV56.localdomain>
2020-06-09 22:15:27 +0200

    Co-authored-by: JonasVinther <jv97@live.dk>

--- config.py
+++ config.py
@@ -14,12 +14,7 @@ conf = {
 }
 
 tsne_params = {
-<<<<<<< HEAD
-    'perplexity':  40,
-    'verbose':     2,  
-=======
     'perplexity':  35,
-    'verbose':      2,
->>>>>>> c1a5fba75ce3170a3b171e6a6455276b1b648da8
+    'verbose':     2,  
     'n_iter':   15000,
 }
