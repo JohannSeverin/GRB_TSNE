fbd3f5249e6c23170ca03bef34c045c1bfdec003
astrockragh <christian.jespersen@nbi.ku.dk>
2020-06-09 20:33:16 +0200

    better plotting fixed

--- plotting.py
+++ plotting.py
@@ -3,12 +3,12 @@ import numpy as np
 from config import conf
 
 embedding = np.genfromtxt('embedding.csv', delimiter=',')
-grbnames = embedding[:,0]
-emb = embedding[:,1:]
+grbnames = embedding[:, 0]
+emb = embedding[:, 1:]
 
 fig, ax = plt.subplots(conf{'figsize'})
 
-emb_plot = ax.scatter(emb[:,0],emb[:,1],s=conf{'radius'},c=conf{'color'},cmap=conf{'cmap'})
-ax.set(xticks=[], yticks=[], )
+emb_plot = ax.scatter(emb[:, 0], emb[:, 1], s=conf{'radius'}, c=conf{'color'}, cmap=conf{'cmap'})
+ax.set(xticks=[], yticks=[])
 
 print('hello')
