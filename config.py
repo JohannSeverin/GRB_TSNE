bf090c8af9a8e6f068f7d9d1288d116b5ebc5633
Vinther901 <jv97@live.dk>
2020-06-09 21:07:49 +0200

    fixed plotting for conf dictionary

--- config.py
+++ config.py
@@ -1,22 +1,13 @@
 import pandas as pd
 import numpy as np
 
-<<<<<<< HEAD
 embedding = np.genfromtxt('embedding.csv', delimiter=',',dtype=str)
 grbnames = embedding[:,0]
-=======
-embedding = np.genfromtxt('embedding.csv', delimiter=',')
-grbnames = embedding[:, 0]
->>>>>>> 64558c756966238067f00b6304c03b5e77bdd856
 duration_data = pd.read_pickle('DataFrames/duration_data.dat')
 
 conf = {
     'figsize': (12, 8),
     'radius': 5,
-<<<<<<< HEAD
-    'color': duration_data.loc[grbnames].T90,
-=======
-    'color': np.log(duration_data.loc[grbnames]),
->>>>>>> 64558c756966238067f00b6304c03b5e77bdd856
+    'color': np.log(duration_data.loc[grbnames].T90),
     'cmap': 'plasma',
 }
--- plotting.py
+++ plotting.py
@@ -2,22 +2,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 from config import conf
 
-<<<<<<< HEAD
 embedding = np.genfromtxt('embedding.csv', delimiter=',',dtype=str)
 grbnames = embedding[:,0]
 emb = embedding[:,1:].astype(float)
-=======
-embedding = np.genfromtxt('embedding.csv', delimiter=',')
-grbnames = embedding[:, 0]
-emb = embedding[:, 1:]
->>>>>>> 64558c756966238067f00b6304c03b5e77bdd856
 
-fig, ax = plt.subplots(figsize = conf{'figsize'})
+fig, ax = plt.subplots(figsize = conf['figsize'])
 
-emb_plot = ax.scatter(emb[:, 0], emb[:, 1], s=conf{'radius'}, c=conf{'color'}, cmap=conf{'cmap'})
+emb_plot = ax.scatter(emb[:, 0], emb[:, 1], s=conf['radius'], c=conf['color'], cmap=conf['cmap'])
 ax.set(xticks=[], yticks=[])
 
-cbar = plt.colorbar(emb_plot,ax)
+
+cbar = plt.colorbar(emb_plot,None,ax)
 
 ax.set(yticks = (), xticks = ())
+
+fig.show()
 
